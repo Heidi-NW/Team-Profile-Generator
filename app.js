e56a5b61a36ac45b8b9c8a9f03f17395e65ec036
Heidi <mcmarketing.ad@gmail.com>
2022-11-06 21:32:59 -0500

    add html & css

--- app.js
+++ app.js
@@ -69,77 +69,77 @@ const employeeQuestions = [{
     when: answers => answers.role === "Intern"
 }]
 
-// getManagerInfo = async (currentId) => {
+getManagerInfo = async (currentId) => {
 
-//     let manager;
+    let manager;
 
-//     try {
-//         await inquirer.prompt(managerQuestions)
-//             .then(answers => {
-//                 const managerName = answers.managerName;
-//                 const managerEmail = answers.managerEmail;
-//                 const managerOfficeNum = answers.managerOfficeNum;
-//                 manager = new Manager(managerName, currentId, managerEmail, managerOfficeNum);
+    try {
+        await inquirer.prompt(managerQuestions)
+            .then(answers => {
+                const managerName = answers.managerName;
+                const managerEmail = answers.managerEmail;
+                const managerOfficeNum = answers.managerOfficeNum;
+                manager = new Manager(managerName, currentId, managerEmail, managerOfficeNum);
 
-//             })
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     return manager;
-// }
+            })
+    } catch (err) {
+        console.log(err);
+    }
+    return manager;
+}
 
-// getEmployeeInfo = async (currentId, team) => {
-//     try {
-//         await inquirer
-//             .prompt(employeeQuestions)
-//             .then(data => {
-//                 currentId++
-//                 const employeeName = data.employeeName;
-//                 const employeeEmail = data.employeeEmail;
-//                 const employeeRole = data.role;
-//                 if (employeeRole == "Engineer") {
-//                     const employeeGithub = data.github;
-//                     const employee = new Engineer(employeeName, currentId, employeeEmail, employeeGithub)
-//                     team.push(employee);
-//                 } else if (employeeRole == "Intern") {
-//                     const employeeSchool = data.school;
-//                     const employee = new Intern(employeeName, currentId, employeeEmail, employeeSchool)
-//                     team.push(employee);
-//                 }
-//                 if (data.addEmployee) {
-//                     console.log("So far, our team looks like this: ")
-//                     console.log(team);
-//                     return getEmployeeInfo(currentId, team);
-//                 } else {
-//                     console.log("All employees added!");
-//                     console.log("The following team was created")
-//                     console.log(team)
-//                 }
-//             })
-//     } catch (err) {
-//         console.log("ERROR!")
-//         console.log(err);
-//     }
-//     return team;
-// }
+getEmployeeInfo = async (currentId, team) => {
+    try {
+        await inquirer
+            .prompt(employeeQuestions)
+            .then(data => {
+                currentId++
+                const employeeName = data.employeeName;
+                const employeeEmail = data.employeeEmail;
+                const employeeRole = data.role;
+                if (employeeRole == "Engineer") {
+                    const employeeGithub = data.github;
+                    const employee = new Engineer(employeeName, currentId, employeeEmail, employeeGithub)
+                    team.push(employee);
+                } else if (employeeRole == "Intern") {
+                    const employeeSchool = data.school;
+                    const employee = new Intern(employeeName, currentId, employeeEmail, employeeSchool)
+                    team.push(employee);
+                }
+                if (data.addEmployee) {
+                    console.log("So far, our team looks like this: ")
+                    console.log(team);
+                    return getEmployeeInfo(currentId, team);
+                } else {
+                    console.log("All employees added!");
+                    console.log("The following team was created")
+                    console.log(team)
+                }
+            })
+    } catch (err) {
+        console.log("ERROR!")
+        console.log(err);
+    }
+    return team;
+}
 
 
-// init = async () => {
-//     let currentId = 1;
-//     let team = [];
-//     const managerInfo = await getManagerInfo(currentId)
-//     console.log("The following is the manager info")
-//     console.log(managerInfo);
-//     team.push(managerInfo);
-//     const employeeInfo = await getEmployeeInfo(currentId, team);
-//     const renderHTML = await render(employeeInfo);
-//     fs.writeFile(outputPath, renderHTML, function (err) {
-//         if (err) {
-//             throw err;
-//         } else {
-//             console.log("The team.html file has been generated!");
-//         }
-//     });
-// };
+init = async () => {
+    let currentId = 1;
+    let team = [];
+    const managerInfo = await getManagerInfo(currentId)
+    console.log("The following is the manager info")
+    console.log(managerInfo);
+    team.push(managerInfo);
+    const employeeInfo = await getEmployeeInfo(currentId, team);
+    const renderHTML = await render(employeeInfo);
+    fs.writeFile(outputPath, renderHTML, function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log("The team.html file has been generated!");
+        }
+    });
+};
 
-// init();
+init();
